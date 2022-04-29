9069b072e59a7c9bf7f970d39887a4b9447e19fc
Thooooor <46197373+Thooooor@users.noreply.github.com>
2022-04-30 01:11:57 +0800

    uodate

--- site.config.ts
+++ site.config.ts
@@ -9,15 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '若虚杂记',
+  domain: 'lizhihao999.cn',
+  author: '若虚',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '在万中走一的路',
 
   // social usernames (optional)
   github: 'Thooooor',
+  zhihu: 'li-zhi-hao-76-18',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -45,16 +46,12 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '383bbe8475af4516aafeda3eb331a78c'
+    },
+  ]
 })
